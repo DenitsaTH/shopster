95fd235e94a58cb4c2d5bc7382e9a10865a3ef36
DenitsaTH <denizza.cristova@gmail.com>
2024-07-23 16:05:35 +0300

    add db connection function

--- app/app.js
+++ app/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import dbConnnect from '../config/dbConnect.js';
 
+dbConnnect(); // db connection
 const app = express(); // create the app - an instance of express
 
 export default app;
