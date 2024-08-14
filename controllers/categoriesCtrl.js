ba98a35130c5bb765856d7dc5cc88759ff612c0e
DenitsaTH <denizza.cristova@gmail.com>
2024-08-14 16:43:49 +0300

    feat: categories crud operations

--- controllers/categoriesCtrl.js
+++ controllers/categoriesCtrl.js
@@ -23,3 +23,59 @@ export const createCategoryCtrl = asyncHandler(async (req, res) => {
         category,
     });
 });
+
+
+export const getCategoriesCtrl = asyncHandler(async (req, res) => {
+    const categories = await Category.find();
+
+    res.json({
+        status: "success",
+        message: 'Categories fetched successfully',
+        categories,
+    });
+});
+
+
+export const getCategoryCtrl = asyncHandler(async (req, res) => {
+    const category = await Category.findById(req.params.id);
+
+    if (!category) {
+        throw new Error('Category not found');
+    }
+
+    res.json({
+        status: 'success',
+        message: 'Category fetched successfully',
+        category,
+    });
+});
+
+
+export const updateCategoryCtrl = asyncHandler(async (req, res) => {
+    const { name } = req.body;
+    
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            name,
+        },
+        {
+            new: true,
+        }
+    );
+    res.json({
+        status: 'success',
+        message: 'Category successfully updated',
+        category,
+    });
+});
+
+
+export const deleteCategoryCtrl = asyncHandler(async (req, res) => {
+    await Category.findByIdAndDelete(req.params.id);
+
+    res.json({
+        status: 'success',
+        message: 'Category deleted successfully'
+    });
+});
--- routes/categoriesRoute.js
+++ routes/categoriesRoute.js
@@ -1,6 +1,10 @@
 import exppress from 'express';
 import {
     createCategoryCtrl,
+    getCategoriesCtrl,
+    getCategoryCtrl,
+    updateCategoryCtrl,
+    deleteCategoryCtrl,
 } from '../controllers/categoriesCtrl.js';
 import isLoggedIn from '../middleware/isLoggedIn.js';
 
@@ -8,7 +12,10 @@ import isLoggedIn from '../middleware/isLoggedIn.js';
 const categoriesRouter = exppress.Router();
 
 categoriesRouter.post('/', isLoggedIn, createCategoryCtrl);
-
+categoriesRouter.get('/', getCategoriesCtrl);
+categoriesRouter.get('/:id', getCategoryCtrl);
+categoriesRouter.put('/:id', isLoggedIn, updateCategoryCtrl);
+categoriesRouter.delete('/:id', isLoggedIn, deleteCategoryCtrl);
 
 
 export default categoriesRouter;
