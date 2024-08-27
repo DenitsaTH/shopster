ca4ef1bb4882a67004700a714225e9e5cee7ab7f
DenitsaTH <denizza.cristova@gmail.com>
2024-08-27 16:04:21 +0300

    feat: add Order model

---
+++ models/Order.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const randomTxt = Math.random().toString(36).substring(7).toLocaleUpperCase();
+const randomNumbers = math.floor(1000 + Math.random() * 90000);
+
+const OrderSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    orderItems: [
+        {
+            type: Object,
+            required: true,
+        },
+    ],
+    shippingAddress: {
+        type: Object,
+        required: true,
+    },
+    orderNumber: {
+        type: String,
+        required: true,
+        default: randomTxt + randomNumbers,
+    },
+    paymentStatus: {
+        type: String,
+        req: true,
+        default: 'Not paid',
+    },
+    paymentMethod: {
+        type: String,
+        default: 'Not specified',
+    },
+    currency:{
+        type: String,
+        default: 'Not paid',
+    },
+    status: {
+        type: String,
+        default: 'pending',
+        enum: ['pending', 'processing', 'shipped', 'delivered'],
+    },
+    deliveredAt: {
+        type: Date, 
+    }, 
+}, {
+    timestamps: true,
+});
+
+
+const Order = mongoose.model('Order', OrderSchema);
+export default Order;
